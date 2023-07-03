1685f0311edaed5ef311533936b347b2249629d9
Rahul <rahulrohilla0204@gmail.com>
2023-07-03 14:50:45 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors') 
+
+connectToMongo();
+const app = express()
+const port = 5000
+
+app.use(cors())
+
+
+// basically a middle to send json files in express request
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+
+app.listen(port, () => {
+  console.log(`Thoughtbook backend listening at http://localhost:${port}`)
+})
